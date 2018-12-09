2890c9777999a566304c114202375945c07b7a5e
sunsijie <sunsijie@buaa.edu.cn>
2018-12-10 00:00:16 +0800

    fix linux

--- scripts/test.cpp
+++ scripts/test.cpp
@@ -22,11 +22,16 @@ void replay()
     uint64_t cur_time = 0;
     uint64_t next_time;
     string line;
-    while (cin >> next_time >> line)
+    char buffer[4096];
+    while (1)
     {
-        usleep(next_time - cur_time);
-        cout << line << endl;
+        cin >> next_time;
+        getchar();
+        usleep((next_time - cur_time));
         cur_time = next_time;
+
+        cin.getline(buffer, 4096, '\n');
+        cout << buffer << endl;
     }
 }
 
